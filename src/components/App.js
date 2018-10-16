a7d76ae853dd347ef07215679f1cba81a888b741
Curtis Wilcox <Curtis.Wilcox@uvm.edu>
2018-10-15 20:59:22 -0400

    cleanup, mobile view update, <Redirect> fix

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import '../styles/App.css';
 import Home from './Home';
 import ShowResult from './ShowResult';
 
-import  {strip} from '../util/helper';
+import {strip} from '../util/helper';
 
 
 // class App extends Component {
@@ -18,98 +18,26 @@ function App(props) {
           exact={true}
           path='/show-dictionary/'
           render={(props) =>
-            <Home
-              {...props}
-              // updateApp={(showname) => {
-              // this.setState({
-              //   chosenShow: showname,
-              // chosenShowInfo: info,
-              // });
-              // }}
-            />
+            <Home{...props}/>
           }
         />
         <Route
           path='/show-dictionary/:name'
           render={(props) => {
             return (
-              <ShowResult
-                {...props}
-                // showname={this.state.chosenShow}
-                // episodes={this.state.chosenShowInfo}
-              />
+              <ShowResult{...props}/>
             );
           }}
         />
         <Route
           path="/:name"
           render={(props) => (
-            <Redirect to={"/show-dictionary" + strip(props.location.pathname)}/>
+            <Redirect to={"/show-dictionary/" + strip(props.location.pathname)}/>
           )}
         />
       </Switch>
     </div>
   );
 }
-
-//   constructor(props) {
-//     super(props);
-//     // this.state = {
-//     // chosenShow: '',
-//     // chosenShowInfo: [],
-//     // }
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//         <Switch>
-//           <Route
-//             exact={true}
-//             path='/'
-//             render={(props) =>
-//               <Home
-//                 {...props}
-//                 // updateApp={(showname) => {
-//                 // this.setState({
-//                 //   chosenShow: showname,
-//                 // chosenShowInfo: info,
-//                 // });
-//                 // }}
-//               />
-//             }
-//           />
-//           <Route
-//             path='/:name'
-//             render={(props) => {
-//               return (
-//                 <ShowResult
-//                   {...props}
-//                   // showname={this.state.chosenShow}
-//                   // episodes={this.state.chosenShowInfo}
-//                 />
-//               );
-//             }}
-//           />
-//         </Switch>
-//       </div>
-//     );
-//
-//     //         {/*<header className='App-header'>*/}
-//     //         {/*<img src={logo} className='App-logo' alt='logo'/>*/}
-//     //         {/*<p>*/}
-//     //         {/*Edit <code>src/App.js</code> and save to reload.*/}
-//     //         {/*</p>*/}
-//     //         {/*<a*/}
-//     //         {/*className='App-link'*/}
-//     //         {/*href='https://reactjs.org'*/}
-//     //         {/*target='_blank'*/}
-//     //         {/*rel='noopener noreferrer'*/}
-//     //         {/*>*/}
-//     //         {/*Learn React*/}
-//     //         {/*</a>*/}
-//     //         {/*</header>*/}
-//   }
-// }
 
 export default App;
